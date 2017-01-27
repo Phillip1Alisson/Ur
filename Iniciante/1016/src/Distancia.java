ec12a972a053b86b0b977cf79294a2a0e4725975
Phillip Alisson Volpi <phill.volpi@gmail.com>
2017-01-27 16:03:18 -0200

    Criado projeto Distancia URI

---
+++ Iniciante/1016/src/Distancia.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Created by Phillip Alisson Volpi on 27/01/17.
+ */
+public class Distancia {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        int distanciaKm =0;
+
+        distanciaKm = entrada.nextInt();
+
+        System.out.println(distanciaKm*2+" minutos");
+    }
+
+}
